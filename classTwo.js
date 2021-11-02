6fc0307e737ca1e2841b2cc6f35ee62e36262ce8
Thomas Chen <thomas.arsenal.chen@gmail.com>
2021-11-02 14:49:42 -0400

    chpt 8

--- classTwo.js
+++ classTwo.js
@@ -48,3 +48,29 @@ values.forEach(value => {
 
 console.log(sum);
 
+
+// chapter 8 word info
+
+const word = prompt("Enter a word:");
+
+console.log(`The word ${word} is ${word.length} characters long`);
+console.log(`Its lowercase value is ${word.toLowerCase()}`);
+console.log(`Its uppercase value is ${word.toUpperCase()}`);
+
+let vowelCount = 0;
+
+[...word].forEach(letter => {
+  const lowerCaseLetter = letter.toLowerCase();
+  if (
+    lowerCaseLetter === "a" ||
+    lowerCaseLetter === "e" ||
+    lowerCaseLetter === "i" ||
+    lowerCaseLetter === "o" ||
+    lowerCaseLetter === "u" ||
+    lowerCaseLetter === "y"
+  ) {
+    vowelCount++;
+  }
+});
+
+console.log(`It has ${vowelCount} vowels`);
