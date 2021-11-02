5ae2f14fcfd234a5602aaecb3023693944f12a08
Thomas Chen <thomas.arsenal.chen@gmail.com>
2021-11-02 01:15:03 -0400

    new exercise added

---
+++ classTwo.js
@@ -0,0 +1,38 @@
+// the js way
+// chapter 6 adding a character experience *tested in jsbin
+
+const aurora = {
+  name: "Aurora",
+  health: 150,
+  strength: 25,
+  xp: 0,
+
+  describe() {
+    return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} XP points`;
+  }
+};
+
+aurora.health -= 20;
+
+aurora.strength += 10;
+
+aurora.xp += 15;
+
+console.log(aurora.describe());
+
+
+
+// chapter 7 Musketeers
+const musketeers = ["Athos", "Porthos", "Aramis"];
+for (let i = 0; i < musketeers.length; i++) {
+  console.log(musketeers[i]);
+}
+musketeers.push("D'Artagnan");
+musketeers.forEach(movie => {
+  console.log(musketeers);
+});
+
+musketeers.splice(1, 1);
+for (const musketeer of musketeers) {
+  console.log(musketeers);
+}
